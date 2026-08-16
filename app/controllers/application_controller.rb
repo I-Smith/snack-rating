90cf721c4bed82f162b2f6c2f3eb6de7cf30891d
Isaac Smith <isaacns98@gmail.com>
2026-08-16 14:03:02 -0400

    Update layout and styling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  :before_action :require_login
+  before_action :require_login
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
